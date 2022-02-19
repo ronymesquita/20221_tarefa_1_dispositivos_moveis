a568929e1e77cb7183c90de7fbe9127ae9cfc65b
Rony Mesquita da Silva <ronymesquitadasilva@gmail.com>
2022-02-18 21:51:50 -0300

    Adição da subtração

--- calculadora.py
+++ calculadora.py
@@ -1,8 +1,15 @@
 import os
+from re import sub
 
 def somar(numero1, numero2):
     return numero1 + numero2
 
+def subtrair(numero1, numero2):
+    return numero1 - numero2
+
 if __name__ == '__main__':
     resultado = somar(2, 3)
+    print(f'Resultado: {resultado}')
+
+    resultado = subtrair(7, 4)
     print(f'Resultado: {resultado}')
